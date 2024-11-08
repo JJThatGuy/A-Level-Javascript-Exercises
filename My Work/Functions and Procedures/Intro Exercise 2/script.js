0bd051a786d9b936f213b6e250cbba166bdd87a5
JJThatGuy <jguy624@canon-slade.bolton.sch.uk>
2024-11-08 12:17:21 +0000

    8-11-24

---
+++ My Work/Functions and Procedures/Intro Exercise 2/script.js
@@ -0,0 +1,12 @@
+// Framework: Procedure to display a welcome message
+function displayWelcomeMessage(name) {
+    // TODO: Display a welcome message like "Hello, [name]!"
+    welcomeMessage.innerHTML = ` hello ${name}`
+}
+
+// Event listener for the button
+document.getElementById('welcomeButton').addEventListener('click', function() {
+    const name = document.getElementById('name').value;
+    // Call the procedure
+    displayWelcomeMessage(name);
+});
